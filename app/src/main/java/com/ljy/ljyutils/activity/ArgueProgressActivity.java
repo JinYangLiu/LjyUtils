1de5187208c0020691a1bf156b39a3b0ba403e0a
ljy <l0751048@163.com>
2018-01-25 14:46:17 +0800

    新增view -仿《最美有物》点赞效果

--- app/src/main/java/com/ljy/ljyutils/activity/ArgueProgressActivity.java
+++ app/src/main/java/com/ljy/ljyutils/activity/ArgueProgressActivity.java
@@ -1,14 +1,11 @@
 package com.ljy.ljyutils.activity;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.widget.CircularProgressDrawable;
-import android.widget.ProgressBar;
 
 import com.ljy.ljyutils.R;
 import com.ljy.ljyutils.base.BaseActivity;
 import com.ljy.view.LjyArgueProgressView;
+import com.ljy.view.LjyZanView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,12 +14,12 @@ public class ArgueProgressActivity extends BaseActivity {
 
     @BindView(R.id.argueProgressView)
     LjyArgueProgressView mLjyArgueProgressView;
-    @BindView(R.id.loading_progress)
-    ProgressBar loadingProgressBar;
-    private int a=2;
-    private int b=3;
-    private String str1="<font color=\"#d50000\">" + "【正方】" + "</font>" +"Android牛";
-    private String str2="<font color=\"#1976d2\">" + "【反方】" + "</font>" +"iOS牛";
+    @BindView(R.id.ljyZanView)
+    LjyZanView ljyZanView;
+    private int a = 1;
+    private int b = 9;
+    private String str1 = "<font color=\"#d50000\">" + "【正方】" + "</font>" + "Android牛";
+    private String str2 = "<font color=\"#1976d2\">" + "【反方】" + "</font>" + "iOS牛";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,35 +27,39 @@ public class ArgueProgressActivity extends BaseActivity {
         setContentView(R.layout.activity_argue_progress);
         ButterKnife.bind(mActivity);
         initView();
+        ljyZanView.setNum(a, b);
+        ljyZanView.setOnClickCallBack(new LjyZanView.OnClickCallBack() {
+            @Override
+            public void listenerLike() {
+                ljyZanView.setNum(++a, b);
+                mLjyArgueProgressView.setUI(a, b, str1, str2);
+            }
+
+            @Override
+            public void listenerDisLike() {
+                ljyZanView.setNum(a, ++b);
+                mLjyArgueProgressView.setUI(a, b, str1, str2);
+            }
+        });
     }
 
     private void initView() {
-        mLjyArgueProgressView.setUI(a,b,str1,str2);
+        mLjyArgueProgressView.setUI(a, b, str1, str2);
         mLjyArgueProgressView.setOnClickListener(new LjyArgueProgressView.OnClickListener() {
             @Override
             public boolean listenerYes() {
-                mLjyArgueProgressView.setUI(++a,b,str1,str2);
+                mLjyArgueProgressView.setUI(++a, b, str1, str2);
                 mLjyArgueProgressView.btnClickable(true);
                 return true;
             }
 
             @Override
             public boolean listenerNo() {
-                mLjyArgueProgressView.setUI(a,++b,str1,str2);
+                mLjyArgueProgressView.setUI(a, ++b, str1, str2);
                 mLjyArgueProgressView.btnClickable(true);
                 return true;
             }
         });
 
-        CircularProgressDrawable drawable=new CircularProgressDrawable(mContext);
-        final Context context = mContext;
-        int[] colorResIds={R.color.thin_red};
-        int[] colorRes = new int[colorResIds.length];
-        for (int i = 0; i < colorResIds.length; i++) {
-            colorRes[i] = ContextCompat.getColor(context, colorResIds[i]);
-        }
-        drawable.setColorSchemeColors(colorRes);
-        drawable.setStyle(CircularProgressDrawable.DEFAULT);
-        loadingProgressBar.setIndeterminateDrawable(drawable);
     }
 }
