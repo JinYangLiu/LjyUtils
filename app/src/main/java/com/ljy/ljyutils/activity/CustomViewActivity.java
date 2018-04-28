b7d09da9ff5b0957ddae5439c8af49dbf11f496f
JinYangLiu <L0751048@163.com>
2018-04-28 14:31:25 +0800

    View移动方式的总结

--- app/src/main/java/com/ljy/ljyutils/activity/CustomViewActivity.java
+++ app/src/main/java/com/ljy/ljyutils/activity/CustomViewActivity.java
@@ -1,15 +1,90 @@
 package com.ljy.ljyutils.activity;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import com.ljy.ljyutils.R;
+import com.ljy.ljyutils.base.BaseActivity;
+import com.ljy.ljyutils.view.CustomView;
+import com.ljy.util.LjyDensityUtil;
+import com.ljy.util.LjyLogUtil;
+import com.ljy.util.LjyViewUtil;
 
-public class CustomViewActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CustomViewActivity extends BaseActivity {
+
+    @BindView(R.id.customView)
+    CustomView customView;
+    @BindView(R.id.view)
+    Button mView;
+    @BindView(R.id.relativeBlue)
+    RelativeLayout relativeBlue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_view);
+
+//        LjyScreenUtils.noStatusBar(mActivity);
+//        LjyScreenUtils.noNavigationBar(mActivity);
+
+        ButterKnife.bind(mActivity);
+        mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LjyLogUtil.i("mView onClick");
+            }
+        });
+        LjyViewUtil.touchMove(mView);
+    }
+
+    private final int count = LjyDensityUtil.dp2px(mContext, 50);
+
+    /**
+     * View移动方式的总结
+     * 1. scroller.startScroll,View.scrollTo,View.scrollBy
+     * (都是对View内容的移动,而view本身并不动,从本例子的View背景并不动就可以看出)
+     * 操作简单,适合对VIew内容的滑动
+     * 2.动画
+     * 操作简单,适用于没有交互的View和实现复杂的动画效果
+     * 3. 改变布局惨呼
+     * 操作稍微复杂,适用于有交互的View
+     *
+     * 弹性滑动的实现方式:Scroller, 动画, Handler.postDelayed,Thread.sleep
+     */
+    public void btnClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn1:
+                customView.smoothScrollTo(count, count);
+                break;
+            case R.id.btn2:
+                customView.scrollTo(count, count);
+                break;
+            case R.id.btn3:
+                customView.scrollBy(count, count);
+                break;
+            case R.id.btn4:
+                ObjectAnimator.ofFloat(customView, "translationY",
+                        0, count).setDuration(800).start();
+                break;
+            case R.id.btn5:
+                ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) customView.getLayoutParams();
+                params.width += count;
+                params.bottomMargin += count;
+                customView.requestLayout();
+                //或者
+//                customView.setLayoutParams(params);
+                break;
+            case R.id.btn6:
+                relativeBlue.setVisibility(relativeBlue.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
+                break;
+
+        }
     }
 }
--- app/src/main/java/com/ljy/ljyutils/view/CustomView.java
+++ app/src/main/java/com/ljy/ljyutils/view/CustomView.java
@@ -2,10 +2,18 @@ package com.ljy.ljyutils.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
+import android.view.ViewConfiguration;
+import android.widget.Scroller;
 
+import com.ljy.ljyutils.R;
 import com.ljy.util.LjyDensityUtil;
 import com.ljy.util.LjyLogUtil;
 import com.ljy.util.LjyScreenUtils;
@@ -14,19 +22,30 @@ import com.ljy.util.LjyScreenUtils;
  * Created by Mr.LJY on 2018/3/26.
  */
 
-public class CustomView extends View {
+public class CustomView extends View implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+    private Scroller scroller;
+    private GestureDetector gestureDetector;
+    //    VelocityTracker:速度追踪
+    private VelocityTracker velocityTracker;
+
     public CustomView(Context context) {
         super(context);
     }
 
     public CustomView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        float dpi= LjyDensityUtil.getDPI(context);
-        LjyLogUtil.i("dpi:"+dpi);
-        int screenWidth= LjyScreenUtils.getScreenWidth(context);
-        LjyLogUtil.i("screenWidth:"+screenWidth);
-        int screenHeight=LjyScreenUtils.getScreenHeight(context);
-        LjyLogUtil.i("screenHeight:"+screenHeight);
+        float dpi = LjyDensityUtil.getDPI(context);
+        LjyLogUtil.i("dpi:" + dpi);
+        int screenWidth = LjyScreenUtils.getScreenWidth(context);
+        LjyLogUtil.i("screenWidth:" + screenWidth);
+        int screenHeight = LjyScreenUtils.getScreenHeight(context);
+        LjyLogUtil.i("screenHeight:" + screenHeight);
+        int touchSlop = ViewConfiguration.get(getContext()).getScaledTouchSlop();
+        LjyLogUtil.i("系统能识别出的滑动最小距离:touchSlop=" + touchSlop);
+
+        gestureDetector = new GestureDetector(getContext(), this);
+
+        scroller = new Scroller(getContext());
     }
 
     /**
@@ -35,15 +54,14 @@ public class CustomView extends View {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-       // LjyLogUtil.i(String.format("--->top_%d,left_%d,bottom_%d,right_%d",getTop(),getLeft(),getBottom(),getRight()));
         // 1. 获取测量模式（Mode）
         int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
-        LjyLogUtil.i("widthSpecMode:"+widthSpecMode);
+        LjyLogUtil.i("widthSpecMode:" + widthSpecMode);
         // 2. 获取测量大小（Size）
         int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
-        LjyLogUtil.i("widthSpecSize :"+widthSpecSize );
+        LjyLogUtil.i("widthSpecSize :" + widthSpecSize);
         // 3. 通过Mode 和 Size 生成新的SpecMode
-        int newWidthSpec=MeasureSpec.makeMeasureSpec(widthSpecSize*2, widthSpecMode);
+        int newWidthSpec = MeasureSpec.makeMeasureSpec(widthSpecSize * 2, widthSpecMode);
 //        super.onMeasure(newWidthSpec, heightMeasureSpec);
 
     }
@@ -54,7 +72,8 @@ public class CustomView extends View {
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
-        LjyLogUtil.i(String.format("changed_%b,top_%d,left_%d,bottom_%d,right_%d",changed,top,left,bottom,right));
+        LjyLogUtil.i(String.format("changed_%b,top_%d,left_%d,bottom_%d,right_%d", changed, top, left, bottom, right));
+        LjyLogUtil.i(String.format("getXX--->top_%d,left_%d,bottom_%d,right_%d", getTop(), getLeft(), getBottom(), getRight()));
     }
 
     /**
@@ -63,6 +82,160 @@ public class CustomView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        int width = getWidth();
+        int height = getHeight();
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(getResources().getColor(R.color.thin_red));
+        canvas.drawCircle(width / 2, height / 2, width / 6, paint);
         LjyLogUtil.i("onDraw");
     }
+
+    @Override
+    public void computeScroll() {
+        if (scroller.computeScrollOffset()) {
+            scrollTo(scroller.getCurrX(), scroller.getCurrY());
+            postInvalidate();
+        }
+    }
+
+    /**
+     * 慢慢滑动到指定位置
+     *
+     * @param destX
+     * @param destY
+     */
+    public void smoothScrollTo(int destX, int destY) {
+
+        int scrollX = getScrollX();
+        LjyLogUtil.i("scrollX:" + scrollX);
+        int scrollY = getScrollY();
+        LjyLogUtil.i("scrollY:" + scrollY);
+        int deltaX = destX - scrollX;
+        int deltaY = destY - scrollY;
+
+        scroller.startScroll(scrollX, scrollY, deltaX, deltaY,900);
+
+        invalidate();
+    }
+
+    private int firstX, firstY, lastX, lastY;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (velocityTracker == null) {
+            velocityTracker = VelocityTracker.obtain();
+        }
+        velocityTracker.addMovement(event);
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                //左上角在父控件中的坐标
+                LjyLogUtil.i("getX:" + getX());
+                LjyLogUtil.i("getY:" + getY());
+                //当前点对与左上角的坐标
+                LjyLogUtil.i("event.getX:" + event.getX());
+                LjyLogUtil.i("event.getY:" + event.getY());
+                //当前点的绝对坐标
+                LjyLogUtil.i("event.rawX:" + event.getRawX());
+                LjyLogUtil.i("event.rawY:" + event.getRawY());
+                LjyLogUtil.i("onTouchEvent.ACTION_DOWN");
+
+                lastX = firstX = (int) event.getX();
+                lastY = firstY = (int) event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                LjyLogUtil.i("onTouchEvent.ACTION_MOVE");
+                scrollTo(firstX-lastX , firstY-lastY );
+                lastX = (int) event.getX();
+                lastY = (int) event.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                LjyLogUtil.i("onTouchEvent.ACTION_UP");
+                velocityTracker.computeCurrentVelocity(1000);//计算1000ms内的速度
+                int xVelocity = (int) velocityTracker.getXVelocity();
+                LjyLogUtil.i("xVelocity:" + xVelocity);
+                int yVelocity = (int) velocityTracker.getYVelocity();
+                LjyLogUtil.i("yVelocity:" + yVelocity);
+                //用完要记得重置回收
+                velocityTracker.clear();
+                velocityTracker.recycle();
+                velocityTracker = null;
+                break;
+        }
+
+        //手势监听
+        return gestureDetector.onTouchEvent(event);
+//        return true;//true,表示不消费,继续传递
+//        return super.onTouchEvent(event);
+    }
+
+    //OnGestureListener start
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        //手指轻触屏幕的一瞬,由ACTION_DOWN触发
+        LjyLogUtil.i("onDown");
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+//        手指轻触屏幕尚未松开或拖动,由ACTION_DOWN触发
+        LjyLogUtil.i("onShowPress");
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        //轻触屏幕后,手指松开,单击行为,由ACTION_UP触发
+        LjyLogUtil.i("onSingleTapUp");
+        return true;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        //手指按下并拖动,由ACTION_DOWN+多个ACTION_MOVE触发
+        LjyLogUtil.i("onScroll");
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        //长按屏幕
+        LjyLogUtil.i("onLongPress");
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        //快速滑动后松开
+        LjyLogUtil.i("onFling");
+        return true;
+    }
+
+    //OnGestureListener end
+
+    //OnDoubleTapListener start
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        //严格的单击行为,不可能是双击中的一次
+        LjyLogUtil.i("onSingleTapConfirmed");
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        //双击
+        LjyLogUtil.i("onDoubleTap");
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        //发生了双击行为(双击期间ACTION_DOWN,ACTION_MOVE,ACTION_UP都会触发此回调)
+        LjyLogUtil.i("onDoubleTapEvent");
+        return true;
+    }
+
+    //OnDoubleTapListener end
+
 }
